ef9db83f6185b24c6e2500cc02c073753c8ed48b
Bob <bobjacob23@yahoo.com>
2024-09-22 23:57:34 -0700

    added inquirer prompt questions

--- index.js
+++ index.js
@@ -5,8 +5,60 @@ import fs from 'fs';
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+  .prompt ([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter a description for your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter the installation instructions",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Enter the usage information",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Enter the contribution guidelines",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Enter tests instructions",
+      name: "tests",
+    },
+    {
+      type: "list",
+      message: "Pick a license for your project:",
+      name: "license",
+      choices: ["MIT", "GPL", "Apache", "BSD"],
+    },
+    {
+      type: "input",
+      message: "Enter GitHub username",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "Enter email address",
+      name: "email",
+    },
+  ])
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile()
+}
 
 // TODO: Create a function to initialize app
 function init() {}
