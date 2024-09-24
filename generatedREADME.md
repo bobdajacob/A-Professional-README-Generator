ff6264bd7cde159a5a6db8c83523fc82951f57e9
Bob <bobjacob23@yahoo.com>
2024-09-24 16:19:26 -0700

    finished renderLicenseLink

--- generatedREADME.md
+++ generatedREADME.md
@@ -1,10 +1,10 @@
 
 # Title
-
-
+title
+![License](https://img.shields.io/badge/License-MIT-yellow)
 
 # Description
-
+project
 
 # Table of Contents
 [Installation](#installation)
@@ -16,25 +16,26 @@
 [Questions](#Questions)
 
 # Installation
-
+install
 
 # Usage
-
+usage
 
 # Contributing
-
+contri
 
 # License
 MIT
 
-# Tests
 
+# Tests
+test
 
 # Technologies Used
-
+HTML,JavaScript,CSS
 
 # Questions reach out to these informations:
-
-
+bob@email.com
+bob
 
 1. Clone the repository:
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,11 +14,20 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== '') {
+    return renderLicenseBadge(license)
+  } else {
+    return '';
+    }
+  }
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 
 // TODO: Create a function to generate markdown for README
@@ -26,7 +35,7 @@ function generateMarkdown(data) {
 return `
 # Title
 ${data.title}
-${renderLicenseBadge(license)}
+${renderLicenseLink(data.license)}
 
 # Description
 ${data.description}
@@ -51,6 +60,7 @@ ${data.contributing}
 
 # License
 ${data.license}
+
 
 # Tests
 ${data.tests}
